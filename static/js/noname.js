af5674814418fc01696fdcd3dfd2cad1c4be56e8
Atsidir <tamas.torjai@gmail.com>
2017-03-13 13:53:53 +0100

    flask, js,index.html implemented 1.0

--- static/js/noname.js
+++ static/js/noname.js
@@ -1,8 +1,45 @@
-// Store
-localStorage.setItem("lastname", "Smith");
-// Retrieve
+function Board(name)
+{
+    this.name=name;
+    this.dict={};
+    this.valami="valami";
+    this.add = function (item) {
+        this.dict[item]="week1";
+    }
+};
+
+function Projects() {
+  this.boardDict = {};
+  this.add= function (name) {
+    this.boardDict[name]=new Board(name);
+  }
+};
+
+var projects=new Projects();
+projects.add("NoName");
+projects.add("SpaceShip");
+projects.add("Codezero");
+projects.add("Nuclear missile");
+localStorage.projects = JSON.stringify(projects);
+
+
+var init= function (boardsObject) {
+    console.log(boardsObject);
+    for (var board in boardsObject.boardDict)
+    {
+
+         $("#projectshere").append("<div class='card'>"+boardsObject.boardDict[board].name+"</div>");
+    };
+
+};
+
+
+
 
 
 $(document).ready(function() {
+    var projects = JSON.parse(localStorage.projects );
+    console.log(projects );
+    init(projects )
 
 });

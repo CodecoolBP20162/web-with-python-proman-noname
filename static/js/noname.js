fefe4abc4218aa334a1d87662097f435d3c9f556
Szeghi Dorottya <szeghi.d@gmail.com>
2017-03-13 15:05:11 +0100

    new board can be added

--- static/js/noname.js
+++ static/js/noname.js
@@ -23,23 +23,47 @@ projects.add("Nuclear missile");
 localStorage.projects = JSON.stringify(projects);
 
 
+
+
 var init= function (boardsObject) {
-    console.log(boardsObject);
+    $("#projectshere").empty();
+    $("#newprojectshere").empty();
+
+
     for (var board in boardsObject.boardDict)
     {
-
          $("#projectshere").append("<div class='card'>"+boardsObject.boardDict[board].name+"</div>");
     };
+    $("#newprojectshere").append("<div class='card'>New Board</div>");
 
 };
 
+var newBoard=function () {
+    document.getElementById('newBoardInput').style.display = 'flex';
+    document.getElementById('save').style.display = 'flex';
+}
+
+var saveNewBoard=function(){
+    var newBoard=$('#newBoardInput').val();
+    var dict = JSON.parse(localStorage.projects );
+    var projects = new Projects();
+    console.log(dict["boardDict"]);
+    projects.boardDict=dict["boardDict"];
+    projects.add(newBoard);
+    localStorage.projects = JSON.stringify(projects);
+    var dict = JSON.parse(localStorage.projects );
+    console.log(dict["boardDict"]);
+    init(dict);
+
+
+}
 
 
 
 
 $(document).ready(function() {
     var projects = JSON.parse(localStorage.projects );
-    console.log(projects );
-    init(projects )
+    console.log("kakis");
+    init(projects );
 
 });

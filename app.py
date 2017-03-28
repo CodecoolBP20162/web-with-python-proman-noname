16cba76b5910434e8dc19f022f76bc653dab4dde
qayol1 <qayol@freemail.hu>
2017-03-28 15:21:27 +0200

    update cell function added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import *
 import example_data
 from build import Build
 from models.status import Status
+from models.cell import Cell
 
 app = Flask(__name__)
 
@@ -19,6 +20,29 @@ def show_status():
     for element in status:
         elements.append(element.status)
     return jsonify(elements)
+
+
+@app.route("/update_data")
+def update_data():
+    result = request.get_json()
+    newid = result["newid"]
+    oldid = result["oldid"]
+    newstatus = result["newstatus"]
+    oldstatus = result["oldstatus"]
+    if (newid != oldid):
+        query = Cell.update(order=newid, status =newstatus).where(Cell.order == oldid and Cell.status == oldstatus)
+        query.execute()
+        query = Cell.update(order=oldid, status=newid).where(Cell.order == newid and Cell.status == newstatus)
+        query.execute()
+
+@app.route("/load_data")
+def load_data():
+    pass
+
+
+@app.route("/save_data")
+def save_data():
+    pass
 
 
 if __name__ == "__main__":

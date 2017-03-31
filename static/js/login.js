ab92fa9e3fae09ab6b193a04dcce6469b4cabb10
Atsidir <tamas.torjai@gmail.com>
2017-03-31 10:18:50 +0200

    game small bug xied

--- static/js/login.js
+++ static/js/login.js
@@ -18,7 +18,39 @@ $(function () {
     });
 });
 */
-console.log('ready');
+function showLogin(){
+    var button=$('#login_btn');
+    switch (button.text()){
+        case "Login":
+            button.text("Cancel");
+            break;
+        case "Cancel":
+            button.text("Login");
+            break;
+    }
+    if ($('#sign_up_btn').text()==="Cancel"){
+        $('#sign_up_btn').text("Sign up");
+        $('#data_for_sign_up').slideToggle("slow");
+    }
+    $('#data_for_login').slideToggle("slow");
+}
+
+function showSignUp(){
+    var button=$('#sign_up_btn');
+    switch (button.text()){
+        case "Sign up":
+            button.text("Cancel");
+            break;
+        case "Cancel":
+            button.text("Sign up");
+            break;
+    }
+    if ($('#login_btn').text()==="Cancel"){
+        $('#login_btn').text("Login");
+        $('#data_for_login').slideToggle("slow");
+    }
+    $('#data_for_sign_up').slideToggle("slow");
+}
 
 
 
